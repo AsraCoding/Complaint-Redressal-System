73ebaaf45779a20c055385d8759acfeb9e2829ab
cutetaps <cutetaps1994@gmail.com>
2023-09-02 00:43:39 +0530

    initial commit

---
+++ ABC_Telecom/src/test/java/com/Simpli/AbcTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simpli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTelecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
